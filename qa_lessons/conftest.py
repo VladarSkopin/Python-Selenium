c3730b38c6ef60c6521c2f91e01f27f62cce2ec0
VladarSkopin <grudagor@gmail.com>
2023-01-07 18:24:12 +0300

    pytest fixtures

---
+++ qa_lessons/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+
+
+# create our own fixtures
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    options.add_argument('chrome')  # Use headless if you do not need a browser UI
+    options.add_argument('--start-maximized')
+    options.add_argument('--window-size=1900,600')
+    return options
+
+
+# "--headless" -> without a UI
+# "--start-maximized" -> regardless of any previous settings
+# "--window-size" -> sets the initial window size
+
+@pytest.fixture
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(executable_path='C:/user/123/...', options=options)  # custom driver path
+    return driver
+
+
+@pytest.fixture(scope='function')  # scope='session' -> for the whole browser, scope='funcion' -> to run in parallel
+def setup(request, get_webdriver):  # request = built-in fixture
+    driver = get_webdriver
+    url = 'https://www.macys.com/'
+    if request.cls is not None:
+        request.cls.driver = driver
+    driver.get(url)  # if request is not a class
+    yield driver
+    # driver.close()  # close browser tab (window)
+    driver.quit()  # close the whole browser
